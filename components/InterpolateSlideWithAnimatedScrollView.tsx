5c9b3c2644d1bf1067fd827427913246531e146c
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-17 15:32:31 +0200

    Add InterpolateSlideWithAnimatedScrollView and SlidePage components

---
+++ components/InterpolateSlideWithAnimatedScrollView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const InterpolateSlideWithAnimatedScrollView = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default InterpolateSlideWithAnimatedScrollView;
---
+++ components/SlidePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const SlidePage = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SlidePage;
