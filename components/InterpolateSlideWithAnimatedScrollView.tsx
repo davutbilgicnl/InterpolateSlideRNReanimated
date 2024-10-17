15443a8d0eaba3bed0d289e1eed283581c5e3a49
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-17 15:52:07 +0200

    Refactor App.tsx and add InterpolateSlideWithAnimatedScrollView and SlidePage components

--- components/InterpolateSlideWithAnimatedScrollView.tsx
+++ components/InterpolateSlideWithAnimatedScrollView.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import SlidePage from './SlidePage';
+
+const IMAGES = [
+  'https://images.unsplash.com/photo-1635492491273-455af7728453?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D',
+  'https://images.unsplash.com/photo-1625014618427-fbc980b974f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D',
+  'https://images.unsplash.com/photo-1617791160588-241658c0f566?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D',
+  'https://images.unsplash.com/photo-1617360547704-3da8b5363369?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D',
+];
 
 const InterpolateSlideWithAnimatedScrollView = () => {
-  return <View></View>;
+  const offsetX = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      offsetX.value = event.contentOffset.x;
+      //   console.log('Scrolling: ', offsetX.value);
+    },
+    onMomentumBegin: (e) => {
+      //   console.log('The list is moving.');
+    },
+    onMomentumEnd: (e) => {
+      //   console.log('The list stopped moving.');
+    },
+  });
+  return (
+    <Animated.ScrollView onScroll={scrollHandler} horizontal style={styles.container} pagingEnabled>
+      {IMAGES.map((url, index) => {
+        return (
+          <Animated.View key={index.toString()}>
+            <SlidePage key={index.toString()} imgurl={url} index={index} offsetX={offsetX} />
+          </Animated.View>
+        );
+      })}
+    </Animated.ScrollView>
+  );
 };
 
-const styles = StyleSheet.create({});
-
 export default InterpolateSlideWithAnimatedScrollView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
--- components/SlidePage.tsx
+++ components/SlidePage.tsx
@@ -1,10 +1,92 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, Platform, StyleSheet, View } from 'react-native';
+import Animated, {
+  Extrapolation,
+  interpolate,
+  SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
-const SlidePage = () => {
-  return <View></View>;
+interface SlidePageProps {
+  imgurl: string;
+  index: number;
+  offsetX?: SharedValue<number>;
+  offsetY?: SharedValue<number>;
+}
+
+const { height, width } = Dimensions.get('window');
+
+const SQUARE_SIZE = width * 0.9;
+
+const SlidePage: React.FC<SlidePageProps> = ({ index, imgurl, offsetX }) => {
+  const inputRange = [(index - 1) * width, index * width, (index + 1) * width];
+
+  const reanimatedStlye = useAnimatedStyle(() => {
+    const scale = interpolate(offsetX?.value!, inputRange, [0, 1, 0], Extrapolation.CLAMP);
+    const borderRadius = interpolate(offsetX?.value!, inputRange, [0, SQUARE_SIZE, 0]);
+
+    const shadowStyle =
+      Platform.OS === 'ios'
+        ? {
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.6,
+            shadowRadius: 6,
+          }
+        : {
+            elevation: 6,
+            shadowColor: '#000',
+          };
+
+    return {
+      borderRadius,
+      transform: [{ scale }],
+      ...shadowStyle,
+    };
+  });
+
+  const reanimatedImageStyle = useAnimatedStyle(() => {
+    const imageSize = interpolate(
+      offsetX?.value!,
+      inputRange,
+      //   [SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE],
+      Array(3).fill(SQUARE_SIZE),
+      Extrapolation.CLAMP
+    );
+
+    return {
+      width: imageSize,
+      height: imageSize,
+    };
+  });
+
+  return (
+    <View style={[styles.container, { backgroundColor: `rgba(255,99,71, 0.2)` }]}>
+      <Animated.View style={[styles.square, reanimatedStlye]} />
+      <Animated.Image
+        style={[styles.image, reanimatedStlye, reanimatedImageStyle]}
+        source={{ uri: imgurl }}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
-
 export default SlidePage;
+
+const styles = StyleSheet.create({
+  container: {
+    height,
+    width,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  square: {
+    height: SQUARE_SIZE,
+    width: SQUARE_SIZE,
+    backgroundColor: 'rgba(0,0,256,0.4)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    position: 'absolute',
+  },
+});
